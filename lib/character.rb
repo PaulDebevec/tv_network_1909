e1b4b7d0797212f7f8b6671ca26b91cc1573286f
Paul Debevec <paulmdebevec@gmail.com>
2019-10-16 09:56:45 -0600

    Initial commit

---
+++ lib/character.rb
@@ -0,0 +1,13 @@
+
+class Character
+  attr_reader :profile, :name, :actor, :salary
+
+  def initialize(profile)
+    @profile = profile
+    @name = profile[:name]
+    @actor = profile[:actor]
+    @salary = profile[:salary]
+  end
+
+
+end
