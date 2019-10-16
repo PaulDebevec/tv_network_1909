7570c154553eb891073b0e7413f96280b91b832a
Paul Debevec <paulmdebevec@gmail.com>
2019-10-16 10:56:56 -0600

    Created test_network_add_show

--- test/network_test.rb
+++ test/network_test.rb
@@ -9,6 +9,14 @@ class NetworkTest < Minitest::Test
 
   def setup
     @nbc = Network.new("NBC")
+
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
   end
 
   def test_it_exists
@@ -18,6 +26,22 @@ class NetworkTest < Minitest::Test
   def test_network_has_a_name
     expected = "NBC"
     actual = @nbc.name
+  end
+
+  def test_network_has_shows
+    expected = []
+    actual = @nbc.shows
+    assert_equal expected, actual
+
+  end
+
+  def test_network_add_show
+    expected = [@parks_and_rec]
+    actual = @nbc.add_show(@parks_and_rec)
+    assert_equal expected, actual
+
+    @nbc.add_show(@knight_rider)
+    assert_equal [@parks_and_rec, @knight_rider]
   end
 
 end
